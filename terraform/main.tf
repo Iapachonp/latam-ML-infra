1095535b8706c2e3873905d72de1ce2d876602ff
Ivan Pachon <Iapachonp@gmail.com>
2023-02-03 14:25:33 -0500

    set up infra 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "tf-state-prod"
+    bucket = "tf-state-prod-a16a0eed-a739-4ff1-b44f-b5a95d54f4df"
     prefix = "terraform/state"
   }
 }
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,6 +1,6 @@
 
 resource "google_storage_bucket" "static-site" {
-  name          = "ml-latam-3F89841D-5689-4FFC-ABED-807239371CA6" # weird name due to uniqueness of bucket name
+  name          = "ml-latam-3f89841d-5689-4ffc-abed-807239371ca6" # weird name due to uniqueness of bucket name
   location      = "EU"
   force_destroy = true
 }

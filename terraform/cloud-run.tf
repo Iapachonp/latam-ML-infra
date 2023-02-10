cadcf0ab3ef135e1bdd42ee98dda3f53e71cda3a
Ivan Pachon <Iapachonp@gmail.com>
2023-02-10 16:03:29 -0500

    port config

--- terraform/cloud-run.tf
+++ terraform/cloud-run.tf
@@ -6,6 +6,7 @@ resource "google_cloud_run_service" "Latam_ML_service" {
     spec {
       containers {
         image = "us-central1-docker.pkg.dev/aa-study/latam-repo/latam-ml-api:production"
+        ports = { containerPort : 8080 }
       }
     }
   }

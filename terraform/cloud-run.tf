ef666d01fcc35945e4d36dc7ba0befcf710a79be
Ivan Pachon <Iapachonp@gmail.com>
2023-02-10 11:52:10 -0500

    try to build service

---
+++ terraform/cloud-run.tf
@@ -0,0 +1,17 @@
+resource "google_cloud_run_service" "Latam_ML_service" {
+  name     = "Latam_ML_service"
+  location = "us-east1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/iapachonp/latam-ml-api"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}

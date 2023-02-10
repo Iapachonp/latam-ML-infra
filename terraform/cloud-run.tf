6985a7d06ea0af830f6ca32f6825f74a23cdbabb
Ivan Pachon <Iapachonp@gmail.com>
2023-02-10 15:17:24 -0500

    build cloud run service

--- terraform/cloud-run.tf
+++ terraform/cloud-run.tf
@@ -1,17 +1,17 @@
-# resource "google_cloud_run_service" "Latam_ML_service" {
-#  name     = "latam-ml-service"
-# location = "us-east1"
-#
-# template {
-#   spec {
-#     containers {
-#       image = "us-docker.pkg.dev/iapachonp/latam-ml-api"
-#     }
-#   }
-# }
+resource "google_cloud_run_service" "Latam_ML_service" {
+  name     = "latam-ml-service"
+  location = "us-east1"
 
-# traffic {
-#   percent         = 100
-#   latest_revision = true
-# }
-# }
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/iapachonp/latam-ml-api"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}

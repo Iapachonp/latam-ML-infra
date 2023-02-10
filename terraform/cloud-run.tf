624967418fb11285aef9318620562295addb57e6
Ivan Pachon <Iapachonp@gmail.com>
2023-02-10 16:31:40 -0500

    port config re-build image

--- terraform/cloud-run.tf
+++ terraform/cloud-run.tf
@@ -1,18 +1,18 @@
-resource "google_cloud_run_service" "Latam_ML_service" {
-  name     = "latam-ml-service"
-  location = "us-east1"
-
-  template {
-    spec {
-      containers {
-        image = "us-central1-docker.pkg.dev/aa-study/latam-repo/latam-ml-api:production"
-        ports { container_port = 8000 }
-      }
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
+# resource "google_cloud_run_service" "Latam_ML_service" {
+#   name     = "latam-ml-service"
+#   location = "us-east1"
+#
+#   template {
+#     spec {
+#       containers {
+#         image = "us-central1-docker.pkg.dev/aa-study/latam-repo/latam-ml-api:production"
+#         ports { container_port = 8000 }
+#       }
+#     }
+#   }
+#
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }

73a0b5e04635e4d2faae41fdcf834a5916c135e1
Ivan Pachon <Iapachonp@gmail.com>
2023-02-10 20:19:51 -0500

    guvicorn idk final

--- entry-point.sh
+++ entry-point.sh
@@ -3,7 +3,7 @@
 getModel(){
   set -e
   model_version=`cat version.txt`
-  if [ "$env"=="local" ] ; then 
+  if [ "$env" == "local" ] ; then 
     cp "/Model/Latam_flight_model.${model_version}.pkl" ./app/
   else
     cp "/Latam_flight_model.${model_version}.pkl" ./app/
@@ -18,5 +18,5 @@ export model_version=`cat version.txt`
 echo "${model_version}"
 # Start service at port 8000
 cd app/src/
-ls
-uvicorn main:app --host 0.0.0.0 --port $PORT
+gunicorn --bind :$PORT --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 8 main:app
+# uvicorn main:app --proxy-headers --host 0.0.0.0 --port 8070
